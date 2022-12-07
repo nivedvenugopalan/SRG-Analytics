3f6e1247f49abb9d7799b8d0f6b7a1e849da9009
Raj Dave <10864@bpsdoha.edu.qa>
2022-12-07 20:01:04 +0300

    add commands cog with one cmd

---
+++ cogs/Commands.py
@@ -0,0 +1,42 @@
+from discord.ext import commands
+
+# Importing our custom variables/functions from backend.py
+from backend import log
+
+
+class Commands(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    # Use @command.Cog.listener() for an event-listener (on_message, on_ready, etc.)
+    @commands.Cog.listener()
+    async def on_ready(self):
+        log.info("Cog: Commands.py Loaded")
+
+    # Use @commands.slash_command() for a slash-command
+    # I recommend using only slash-commands for your bot.
+    @commands.slash_command(name="harvest", description="Harvests data from a guild.")
+    async def harvest(self, ctx):
+        guild = ctx.guild
+
+        data_list = {}
+
+        # Iterate through all channels in the guild
+        for channel in guild.channels:
+            # Iterate through all messages in the channel
+            data_list[channel.name] = []
+
+            async for message in channel.history(limit=None):
+                data_list[channel.name].append([message.id, message.content, message.author.id, message.created_at, message.reference.message_id if message.reference else None])
+
+        # Add the data to the database
+        for channel in data_list:
+            self.manager.add_bulk_data(guild.id, data_list[channel])
+
+
+# The `setup` function is required for the cog to work
+# Don't change anything in this function, except for the
+# name of the cog (Example) to the name of your class.
+def setup(client):
+    # Here, `Example` is the name of the class
+    client.add_cog(Commands(client))
