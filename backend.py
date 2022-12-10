48192d777a83dfe9b9b884a65f3529b8b189d2b4
Nived Venugopalan <nivedprogramming@gmail.com>
2022-12-10 17:59:08 +0530

    added missing code

--- backend.py
+++ backend.py
@@ -239,3 +239,59 @@ class DataManager:
         log.debug(rtn)
 
         return rtn
+
+    def _net_polarity (self, guild_id:int, author_id:int, verbose=0, msg_cache=None):
+        messages = self._get_all_messages(guild_id, author_id) if msg_cache is None else msg_cache
+        number_of_messages = len(messages)
+        MESSAGE = ".\n".join(messages)
+
+        polarity = round((TextBlob(MESSAGE).sentiment.polarity/number_of_messages)*10000,4)
+
+        if verbose != 0:
+            log.debug(polarity)
+
+        return polarity
+
+    def _total_mentions(self, guild_id:int, author_id:int):
+        self.cur.execute("SELECT mentions FROM `{}` WHERE author_id={};".format(str(guild_id), author_id))
+        messages = self.cur.fetchall()
+
+        return len(messages)
+
+    def _most_mentioned_person(self, guild_id:int, author_id:int):
+        self.cur.execute("SELECT mentions FROM `{}` WHERE author_id={} AND ctx_id IS NULL AND mentions IS NOT NULL;".format(str(guild_id), author_id))
+        messages = self.cur.fetchall()
+
+        mentions = []
+        for mention in messages:
+            if mention[0] is not None:
+                lst = mention[0].strip('][').split(', ')
+                for id_ in lst:
+                    mentions.append(int(id_))
+
+        freq = collections.Counter(mentions)
+        return freq.most_common(1)
+
+    def _total_times_mentioned_and_by_who(self, guild_id:int, author_id:int):
+        self.cur.execute("SELECT author_id FROM `{}` WHERE ctx_id IS NULL AND mentions={};".format(str(guild_id), author_id))
+        ids_ = self.cur.fetchall()
+
+        return len(ids_),collections.Counter(ids_).most_common(1) 
+    
+
+class Profile():
+    def __init__(self, guild_id:int, ID:int, messages:list, top_2_words:list, net_polarity:int, total_mentions:int, most_mentioned_person_id:int, total_times_mentioned:int, most_mentioned_by_id:int) -> None:
+        self.guildID = guild_id # to be removed in the future
+        self.ID = ID
+
+        # NLP
+        self.messages = messages
+        self.number_of_messages = len(messages)
+        self.top_2_words = top_2_words
+        self.net_polarity = net_polarity
+
+        # Mentions
+        self.total_mentions = total_mentions
+        self.most_mentioned_person_id = most_mentioned_person_id
+        self.total_times_mentioned = total_times_mentioned
+        self.most_mentioned_by_id = most_mentioned_by_id
