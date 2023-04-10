8c0e81f8afca1b797ca32b3c5f1ec76682da8796
Nived Venugopalan <nivedprogramming@gmail.com>
2023-04-10 13:48:19 +0530

    Clean Slate

--- mat/entity_recognition.py
+++
@@ -1,6 +0,0 @@
-from mat.mat import nlp
-
-
-def get_entities(text: str) -> list[tuple[str, str]]:
-    doc = nlp(text)
-    return [(ent.text, ent.label_) for ent in doc.ents]
--- mat/sentiment_analysis.py
+++
@@ -1,6 +0,0 @@
-from mat.mat import nlp
-
-
-def analyze_sentiment(text: str) -> float:
-    doc = nlp(text)
-    return (doc.sentiment)
